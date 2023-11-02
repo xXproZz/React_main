a1589a95f623b6bbc9fb0782853bc6cf133365f0
Juan Pires Souza <139834779+xXproZz@users.noreply.github.com>
2023-11-02 19:24:45 -0300

    Add files via upload

---
+++ 2_FUNDAMENTOS/fundamentos_react/src/components/FirstComponent.jsx
@@ -0,0 +1,13 @@
+import MyComponent from "./MyComponent";
+
+// 1 - criando componente
+const FirstComponent = () => {
+  return (
+    <div>
+      <h2>Meu primeiro componente!</h2>
+      <MyComponent />
+    </div>
+  );
+};
+
+export default FirstComponent;
